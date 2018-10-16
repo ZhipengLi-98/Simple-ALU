0cd0122d9a8b668c89b6e94102940d3d36c14e3d
黄冰鉴 <2016011296@secoder.net>
2018-10-16 19:28:04 +0800

    feat: add flags and 5 ops
    flags are defined.
    ADD SUB AND OR XOR is added.

--- thinpad_top.runs/pll_example_synth_1/pll_example.tcl
+++ thinpad_top.runs/pll_example_synth_1/pll_example.tcl
@@ -17,7 +17,6 @@ proc create_report { reportName command } {
     send_msg_id runtcl-5 warning "$msg"
   }
 }
-set_param xicom.use_bs_reader 1
 set_param project.vivado.isBlockSynthRun true
 set_msg_config -msgmgr_mode ooc_run
 create_project -in_memory -part xc7a100tfgg676-2L
@@ -26,14 +25,17 @@ set_param project.singleFileAddWarning.threshold 0
 set_param project.compositeFile.enableAutoGeneration 0
 set_param synth.vivado.isSynthRun true
 set_msg_config -source 4 -id {IP_Flow 19-2162} -severity warning -new_severity info
-set_property webtalk.parent_dir C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.cache/wt [current_project]
-set_property parent.project_path C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.xpr [current_project]
+set_property webtalk.parent_dir D:/2018autumn/organization/Simple-ALU/thinpad_top.cache/wt [current_project]
+set_property parent.project_path D:/2018autumn/organization/Simple-ALU/thinpad_top.xpr [current_project]
 set_property XPM_LIBRARIES XPM_CDC [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
-set_property ip_output_repo c:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.cache/ip [current_project]
+set_property ip_output_repo d:/2018autumn/organization/Simple-ALU/thinpad_top.cache/ip [current_project]
 set_property ip_cache_permissions {read write} [current_project]
-read_ip -quiet C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.srcs/sources_1/ip/pll_example/pll_example.xci
+read_ip -quiet D:/2018autumn/organization/Simple-ALU/thinpad_top.srcs/sources_1/ip/pll_example/pll_example.xci
+set_property used_in_implementation false [get_files -all d:/2018autumn/organization/Simple-ALU/thinpad_top.srcs/sources_1/ip/pll_example/pll_example_board.xdc]
+set_property used_in_implementation false [get_files -all d:/2018autumn/organization/Simple-ALU/thinpad_top.srcs/sources_1/ip/pll_example/pll_example.xdc]
+set_property used_in_implementation false [get_files -all d:/2018autumn/organization/Simple-ALU/thinpad_top.srcs/sources_1/ip/pll_example/pll_example_ooc.xdc]
 
 # Mark all dcp files as not used in implementation to prevent them from being
 # stitched into the results of this synthesis run. Any black boxes in the
@@ -43,9 +45,11 @@ read_ip -quiet C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpa
 foreach dcp [get_files -quiet -all -filter file_type=="Design\ Checkpoint"] {
   set_property used_in_implementation false $dcp
 }
+read_xdc dont_touch.xdc
+set_property used_in_implementation false [get_files dont_touch.xdc]
 set_param ips.enableIPCacheLiteLoad 0
 
-set cached_ip [config_ip_cache -export -no_bom -use_project_ipc -dir C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.runs/pll_example_synth_1 -new_name pll_example -ip [get_ips pll_example]]
+set cached_ip [config_ip_cache -export -no_bom -use_project_ipc -dir D:/2018autumn/organization/Simple-ALU/thinpad_top.runs/pll_example_synth_1 -new_name pll_example -ip [get_ips pll_example]]
 
 if { $cached_ip eq {} } {
 close [open __synthesis_is_running__ w]
@@ -86,32 +90,32 @@ write_checkpoint -force -noxdef pll_example.dcp
 create_report "pll_example_synth_1_synth_report_utilization_0" "report_utilization -file pll_example_utilization_synth.rpt -pb pll_example_utilization_synth.pb"
 
 if { [catch {
-  file copy -force C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.runs/pll_example_synth_1/pll_example.dcp C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.srcs/sources_1/ip/pll_example/pll_example.dcp
+  file copy -force D:/2018autumn/organization/Simple-ALU/thinpad_top.runs/pll_example_synth_1/pll_example.dcp D:/2018autumn/organization/Simple-ALU/thinpad_top.srcs/sources_1/ip/pll_example/pll_example.dcp
 } _RESULT ] } { 
   send_msg_id runtcl-3 error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
   error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
 }
 
 if { [catch {
-  write_verilog -force -mode synth_stub C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.srcs/sources_1/ip/pll_example/pll_example_stub.v
+  write_verilog -force -mode synth_stub D:/2018autumn/organization/Simple-ALU/thinpad_top.srcs/sources_1/ip/pll_example/pll_example_stub.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a Verilog synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  write_vhdl -force -mode synth_stub C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.srcs/sources_1/ip/pll_example/pll_example_stub.vhdl
+  write_vhdl -force -mode synth_stub D:/2018autumn/organization/Simple-ALU/thinpad_top.srcs/sources_1/ip/pll_example/pll_example_stub.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a VHDL synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  write_verilog -force -mode funcsim C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.srcs/sources_1/ip/pll_example/pll_example_sim_netlist.v
+  write_verilog -force -mode funcsim D:/2018autumn/organization/Simple-ALU/thinpad_top.srcs/sources_1/ip/pll_example/pll_example_sim_netlist.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the Verilog functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
 
 if { [catch {
-  write_vhdl -force -mode funcsim C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.srcs/sources_1/ip/pll_example/pll_example_sim_netlist.vhdl
+  write_vhdl -force -mode funcsim D:/2018autumn/organization/Simple-ALU/thinpad_top.srcs/sources_1/ip/pll_example/pll_example_sim_netlist.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the VHDL functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
@@ -121,47 +125,47 @@ if { [catch {
 
 
 if { [catch {
-  file copy -force C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.runs/pll_example_synth_1/pll_example.dcp C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.srcs/sources_1/ip/pll_example/pll_example.dcp
+  file copy -force D:/2018autumn/organization/Simple-ALU/thinpad_top.runs/pll_example_synth_1/pll_example.dcp D:/2018autumn/organization/Simple-ALU/thinpad_top.srcs/sources_1/ip/pll_example/pll_example.dcp
 } _RESULT ] } { 
   send_msg_id runtcl-3 error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
   error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
 }
 
 if { [catch {
-  file rename -force C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.runs/pll_example_synth_1/pll_example_stub.v C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.srcs/sources_1/ip/pll_example/pll_example_stub.v
+  file rename -force D:/2018autumn/organization/Simple-ALU/thinpad_top.runs/pll_example_synth_1/pll_example_stub.v D:/2018autumn/organization/Simple-ALU/thinpad_top.srcs/sources_1/ip/pll_example/pll_example_stub.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a Verilog synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  file rename -force C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.runs/pll_example_synth_1/pll_example_stub.vhdl C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.srcs/sources_1/ip/pll_example/pll_example_stub.vhdl
+  file rename -force D:/2018autumn/organization/Simple-ALU/thinpad_top.runs/pll_example_synth_1/pll_example_stub.vhdl D:/2018autumn/organization/Simple-ALU/thinpad_top.srcs/sources_1/ip/pll_example/pll_example_stub.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a VHDL synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  file rename -force C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.runs/pll_example_synth_1/pll_example_sim_netlist.v C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.srcs/sources_1/ip/pll_example/pll_example_sim_netlist.v
+  file rename -force D:/2018autumn/organization/Simple-ALU/thinpad_top.runs/pll_example_synth_1/pll_example_sim_netlist.v D:/2018autumn/organization/Simple-ALU/thinpad_top.srcs/sources_1/ip/pll_example/pll_example_sim_netlist.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the Verilog functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
 
 if { [catch {
-  file rename -force C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.runs/pll_example_synth_1/pll_example_sim_netlist.vhdl C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.srcs/sources_1/ip/pll_example/pll_example_sim_netlist.vhdl
+  file rename -force D:/2018autumn/organization/Simple-ALU/thinpad_top.runs/pll_example_synth_1/pll_example_sim_netlist.vhdl D:/2018autumn/organization/Simple-ALU/thinpad_top.srcs/sources_1/ip/pll_example/pll_example_sim_netlist.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the VHDL functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
 
 }; # end if cached_ip 
 
-if {[file isdir C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.ip_user_files/ip/pll_example]} {
+if {[file isdir D:/2018autumn/organization/Simple-ALU/thinpad_top.ip_user_files/ip/pll_example]} {
   catch { 
-    file copy -force C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.srcs/sources_1/ip/pll_example/pll_example_stub.v C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.ip_user_files/ip/pll_example
+    file copy -force D:/2018autumn/organization/Simple-ALU/thinpad_top.srcs/sources_1/ip/pll_example/pll_example_stub.v D:/2018autumn/organization/Simple-ALU/thinpad_top.ip_user_files/ip/pll_example
   }
 }
 
-if {[file isdir C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.ip_user_files/ip/pll_example]} {
+if {[file isdir D:/2018autumn/organization/Simple-ALU/thinpad_top.ip_user_files/ip/pll_example]} {
   catch { 
-    file copy -force C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.srcs/sources_1/ip/pll_example/pll_example_stub.vhdl C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.ip_user_files/ip/pll_example
+    file copy -force D:/2018autumn/organization/Simple-ALU/thinpad_top.srcs/sources_1/ip/pll_example/pll_example_stub.vhdl D:/2018autumn/organization/Simple-ALU/thinpad_top.ip_user_files/ip/pll_example
   }
 }
 file delete __synthesis_is_running__
--- thinpad_top.runs/pll_example_synth_1/runme.sh
+++ thinpad_top.runs/pll_example_synth_1/runme.sh
@@ -11,9 +11,9 @@ echo "Please update the PATH and LD_LIBRARY_PATH variables below, before executi
 exit
 
 if [ -z "$PATH" ]; then
-  PATH=D:/Xilinx/SDK/2018.2/bin;D:/Xilinx/Vivado/2018.2/ids_lite/ISE/bin/nt64;D:/Xilinx/Vivado/2018.2/ids_lite/ISE/lib/nt64:D:/Xilinx/Vivado/2018.2/bin
+  PATH=D:/programs/Xilinx/SDK/2018.2/bin;D:/programs/Xilinx/Vivado/2018.2/ids_lite/ISE/bin/nt64;D:/programs/Xilinx/Vivado/2018.2/ids_lite/ISE/lib/nt64:D:/programs/Xilinx/Vivado/2018.2/bin
 else
-  PATH=D:/Xilinx/SDK/2018.2/bin;D:/Xilinx/Vivado/2018.2/ids_lite/ISE/bin/nt64;D:/Xilinx/Vivado/2018.2/ids_lite/ISE/lib/nt64:D:/Xilinx/Vivado/2018.2/bin:$PATH
+  PATH=D:/programs/Xilinx/SDK/2018.2/bin;D:/programs/Xilinx/Vivado/2018.2/ids_lite/ISE/bin/nt64;D:/programs/Xilinx/Vivado/2018.2/ids_lite/ISE/lib/nt64:D:/programs/Xilinx/Vivado/2018.2/bin:$PATH
 fi
 export PATH
 
@@ -24,7 +24,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.runs/pll_example_synth_1'
+HD_PWD='D:/2018autumn/organization/Simple-ALU/thinpad_top.runs/pll_example_synth_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
