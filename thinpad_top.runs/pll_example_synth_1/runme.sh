99db95c6018cc811bb9048ddcb61571e6ba10443
LJSthu <1336105717@qq.com>
2018-10-16 22:58:05 +0800

    feat: implement the alu in 5.4

--- thinpad_top.runs/pll_example_synth_1/runme.sh
+++ thinpad_top.runs/pll_example_synth_1/runme.sh
@@ -11,9 +11,9 @@ echo "Please update the PATH and LD_LIBRARY_PATH variables below, before executi
 exit
 
 if [ -z "$PATH" ]; then
-  PATH=D:/Xilinx/SDK/2018.2/bin;D:/Xilinx/Vivado/2018.2/ids_lite/ISE/bin/nt64;D:/Xilinx/Vivado/2018.2/ids_lite/ISE/lib/nt64:D:/Xilinx/Vivado/2018.2/bin
+  PATH=C:/Xilinx/SDK/2018.2/bin;C:/Xilinx/Vivado/2018.2/ids_lite/ISE/bin/nt64;C:/Xilinx/Vivado/2018.2/ids_lite/ISE/lib/nt64:C:/Xilinx/Vivado/2018.2/bin
 else
-  PATH=D:/Xilinx/SDK/2018.2/bin;D:/Xilinx/Vivado/2018.2/ids_lite/ISE/bin/nt64;D:/Xilinx/Vivado/2018.2/ids_lite/ISE/lib/nt64:D:/Xilinx/Vivado/2018.2/bin:$PATH
+  PATH=C:/Xilinx/SDK/2018.2/bin;C:/Xilinx/Vivado/2018.2/ids_lite/ISE/bin/nt64;C:/Xilinx/Vivado/2018.2/ids_lite/ISE/lib/nt64:C:/Xilinx/Vivado/2018.2/bin:$PATH
 fi
 export PATH
 
@@ -24,7 +24,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.runs/pll_example_synth_1'
+HD_PWD='C:/Users/liujiashuo/Desktop/Simple-ALU/thinpad_top.runs/pll_example_synth_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
--- thinpad_top.runs/synth_1/thinpad_top.tcl
+++ thinpad_top.runs/synth_1/thinpad_top.tcl
@@ -17,27 +17,25 @@ proc create_report { reportName command } {
     send_msg_id runtcl-5 warning "$msg"
   }
 }
-set_param xicom.use_bs_reader 1
 create_project -in_memory -part xc7a100tfgg676-2L
 
 set_param project.singleFileAddWarning.threshold 0
 set_param project.compositeFile.enableAutoGeneration 0
 set_param synth.vivado.isSynthRun true
 set_msg_config -source 4 -id {IP_Flow 19-2162} -severity warning -new_severity info
-set_property webtalk.parent_dir C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.cache/wt [current_project]
-set_property parent.project_path C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.xpr [current_project]
+set_property webtalk.parent_dir C:/Users/liujiashuo/Desktop/Simple-ALU/thinpad_top.cache/wt [current_project]
+set_property parent.project_path C:/Users/liujiashuo/Desktop/Simple-ALU/thinpad_top.xpr [current_project]
 set_property XPM_LIBRARIES XPM_CDC [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
-set_property ip_output_repo c:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.cache/ip [current_project]
+set_property ip_output_repo c:/Users/liujiashuo/Desktop/Simple-ALU/thinpad_top.cache/ip [current_project]
 set_property ip_cache_permissions {read write} [current_project]
 read_verilog -library xil_defaultlib {
-  C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.srcs/sources_1/new/SEG7_LUT.v
-  C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.srcs/sources_1/new/async.v
-  C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.srcs/sources_1/new/vga.v
-  C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.srcs/sources_1/new/thinpad_top.v
+  C:/Users/liujiashuo/Desktop/Simple-ALU/thinpad_top.srcs/sources_1/new/async.v
+  C:/Users/liujiashuo/Desktop/Simple-ALU/thinpad_top.srcs/sources_1/new/vga.v
+  C:/Users/liujiashuo/Desktop/Simple-ALU/thinpad_top.srcs/sources_1/new/thinpad_top.v
 }
-read_ip -quiet C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.srcs/sources_1/ip/pll_example/pll_example.xci
+read_ip -quiet C:/Users/liujiashuo/Desktop/Simple-ALU/thinpad_top.srcs/sources_1/ip/pll_example/pll_example.xci
 
 # Mark all dcp files as not used in implementation to prevent them from being
 # stitched into the results of this synthesis run. Any black boxes in the
@@ -47,8 +45,8 @@ read_ip -quiet C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpa
 foreach dcp [get_files -quiet -all -filter file_type=="Design\ Checkpoint"] {
   set_property used_in_implementation false $dcp
 }
-read_xdc C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.srcs/constrs_1/new/thinpad_top.xdc
-set_property used_in_implementation false [get_files C:/Users/19081/Desktop/大三秋季学期/计原/thinpad_top-rev.3/thinpad_top-rev.3/thinpad_top.srcs/constrs_1/new/thinpad_top.xdc]
+read_xdc C:/Users/liujiashuo/Desktop/Simple-ALU/thinpad_top.srcs/constrs_1/new/thinpad_top.xdc
+set_property used_in_implementation false [get_files C:/Users/liujiashuo/Desktop/Simple-ALU/thinpad_top.srcs/constrs_1/new/thinpad_top.xdc]
 
 set_param ips.enableIPCacheLiteLoad 0
 close [open __synthesis_is_running__ w]
